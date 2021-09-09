9e2391c3740a59f5bb69d6333809d585bbd221b3
michihiro <pomeofishbird0303@gmail.com>
2021-09-10 08:31:47 +0900

    詳細ページのswiper導入、スタイリング

--- assets/application.js
+++ assets/application.js
@@ -1,1 +1,17 @@
 // Put your application javascript here
+const store = Vue.reactive({
+    state: {
+        cartState: []
+    },
+    async getCart() {
+        try {
+            const res = await axios.get("/cart.js")
+            this.state.cartState.unShift(res.data)
+        } catch (e) {
+            console.error(e)
+        }
+
+    }
+})
+
+const miniCartState
