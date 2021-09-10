5e6ffee43f711818fe7df81ebadc4a01cab31fe9
michihiro <pomeofishbird0303@gmail.com>
2021-09-10 10:29:19 +0900

    branch内容と違い セレクトメニューがひとつしか無い場合は非表示にする機能の実装

--- assets/application.js
+++ assets/application.js
@@ -14,4 +14,31 @@ const store = Vue.reactive({
     }
 })
 
-const miniCartState
+const miniCartState = Vue.reactive({
+    hidden: true
+})
+
+const toggleMiniCart = {
+    openCart() {
+        miniCartState.hidden = !miniCartState.hidden
+        menuState.hidden = true
+    }
+}
+
+const menuState = Vue.reactive({
+    hidden: true
+})
+
+const toggleMenu = {
+    openMenu() {
+        menuState.hidden = !menuState.hidden
+        miniCartState.hidden = true
+    }
+}
+
+const toggleClose = {
+    closeAll() {
+        miniCartState.hidden = true
+        menuState.hidden = true
+    }
+}
